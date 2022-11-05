4a333bbadb8610ac44cf5a5045ca208f4d799378
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-11-05 01:51:42 +0000

    format

--- shipit/shipit_stack.py
+++ shipit/shipit_stack.py
@@ -18,11 +18,11 @@ class ShipitStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         Aspects.of(self).add(
-             cdk_nag.AwsSolutionsChecks(
-                 log_ignores = True,
-                 verbose = True
-             )
-         )
+            cdk_nag.AwsSolutionsChecks(
+                log_ignores = True,
+                verbose = True
+            )
+        )
 
         cdk_nag.NagSuppressions.add_stack_suppressions(
             self, suppressions = [
